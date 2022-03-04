c01f70a16ee9f2868755d9a327e7eae8e8da76e7
Andrej Simeunovic <andrej.simeunovic98@gmail.com>
2022-03-04 13:45:42 +0100

    added routes

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DisplayMoviesComponent } from './components/display-movies/display-movies.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: AppComponent},
+                         {path:"movie-details/:imdbID", component: MovieDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/components/movie-details/movie-details.component.ts
+++ movie-app/src/app/components/movie-details/movie-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movies } from 'src/app/Movie';
+
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+  //@Input() movies: Movies
+  
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+  const routeParams = this.route.snapshot.paramMap;
+  const productIdFromRoute = String(routeParams.get('imdbID'));
+
+  // Find the product that correspond with the id provided in route.
+ // this.movies = this.movies.find(movie => movie.imdbID === productIdFromRoute);
   }
 
 }
