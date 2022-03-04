38fb91e9add4810c796662094e47409e5c58d3c1
MediaMarktLuffare <uros_1997@hotmail.com>
2022-03-04 14:06:57 +0100

    Router works

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DisplayMoviesComponent } from './components/display-movies/display-movies.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-const routes: Routes = [{path: '', component: AppComponent},
-                         {path:"movie-details/:imdbID", component: MovieDetailsComponent}];
+const routes: Routes = [
+  {path: '', component: DisplayMoviesComponent},
+  {path:"movie-details/:imdbID", component: MovieDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/components/movie-details/movie-details.component.ts
+++ movie-app/src/app/components/movie-details/movie-details.component.ts
@@ -10,17 +10,11 @@ import { Movies } from 'src/app/Movie';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  //@Input() movies: Movies
+  movies!: Movies;
   
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // First get the product id from the current route.
-  const routeParams = this.route.snapshot.paramMap;
-  const productIdFromRoute = String(routeParams.get('imdbID'));
-
-  // Find the product that correspond with the id provided in route.
- // this.movies = this.movies.find(movie => movie.imdbID === productIdFromRoute);
   }
 
 }
