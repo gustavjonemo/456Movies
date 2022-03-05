86babef6da7c74f320a5cfedbe139e16f1cd6d22
MediaMarktLuffare <uros_1997@hotmail.com>
2022-03-05 14:24:33 +0100

    Fixed Routing

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisplayMoviesComponent } from './components/display-movies/display-movies.component';
+import { FilterComponent } from './components/filter/filter.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 //Lägg in här om det skall komma in flera routes, renderas i <router-outlet>
 const routes: Routes = [
-  {path: '', component: DisplayMoviesComponent},
+  {path: '', component: FilterComponent},
   {path:"movie-details/:imdbID", component: MovieDetailsComponent}
 ];
 
